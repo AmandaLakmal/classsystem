c2b6a595b9d585112ddca60059bf77c5f9654f01
AmandaLakmal <lakmalamanda@gmail.com>
2026-05-15 09:31:05 +0530

    Initial commit

---
+++ src/main/java/com/lms/classsystem/ClasssystemApplication.java
@@ -0,0 +1,13 @@
+package com.lms.classsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasssystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasssystemApplication.class, args);
+	}
+
+}
