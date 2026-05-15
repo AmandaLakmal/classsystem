0c885411c9f79a1b2c42d87873abb7951675bebb
AmandaLakmal <lakmalamanda@gmail.com>
2026-05-15 15:13:25 +0530

    Added Instructor Enrollment and repository"

---
+++ src/main/java/com/lms/classsystem/entity/Enrollment.java
@@ -0,0 +1,29 @@
+package com.lms.classsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "enrollement")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Enrollment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    private String lastPaidMonth;  //eg :- may 20
+    private boolean hasAccess; //if student complete his payment show content
+}
